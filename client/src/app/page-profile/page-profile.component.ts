8c67358ae61245d79ff019292f7c2eb4661b4254
vesnanervah <c.zwerew2012@gmail.com>
2023-11-17 11:36:54 +0300

    feat: add home page text

--- client/src/app/page-profile/page-profile.component.ts
+++ client/src/app/page-profile/page-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
